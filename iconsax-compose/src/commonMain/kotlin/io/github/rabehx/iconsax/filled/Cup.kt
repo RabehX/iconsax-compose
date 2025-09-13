20170e8ad90f1eecf9fbee95431c2193f5e1c66e
FegzDev <fegzdev@gmail.com>
2025-09-13 11:55:05 +0100

    Migrate Project to Compose Multiplatform.

--- iconsax-compose/src/commonMain/kotlin/io/github/rabehx/iconsax/filled/Cup.kt
+++ iconsax-compose/src/commonMain/kotlin/io/github/rabehx/iconsax/filled/Cup.kt
@@ -19,6 +19,27 @@
  *
  */
 
+package io.github.rabehx.iconsax.filled/*
+ *
+ *  * Copyright (c) 2025, Рабeх Аззам
+ *  *
+ *  * This file is part of Iconsax Compose.
+ *  *
+ *  * Iconsax Compose is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * Iconsax Compose is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with Iconsax Compose. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
